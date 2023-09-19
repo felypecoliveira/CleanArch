d26fc654552bcc4e87ead09ac706efc5181951cb
Usuário <andrefelype2015@gmail.com>
2023-09-19 00:00:05 -0300

    refactor: adding some new lines

--- src/adapter/adpt_contatos/pg_contatos.py
+++ src/adapter/adpt_contatos/pg_contatos.py
@@ -9,6 +9,20 @@ class PostgresAdptContatos:
     def __init__(self, db_contatos: DbActionsContatos):
         self.db_contatos = db_contatos
 
+    def add_contato_to_cliente(self):
+        pass
+
+    def update_contato(self):
+        pass
+
+    def get_contatos(self):
+        pass
+
+    def delete_contato(self):
+        pass
+
+    def select_contato(self):
+        pass
 
 def ioc_config_contatos(binder):
     binder.bind(DbInterfaceContatos, ContatosUseCase)
