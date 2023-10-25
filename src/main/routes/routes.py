6b26cf16663817818672ded52d40a6154db7250c
Usuário <andrefelype2015@gmail.com>
2023-10-25 00:29:59 -0300

    feat: more comments, format the code

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -5,19 +5,20 @@ from flask import Blueprint, request, jsonify
 from src.main.adapters.request_adapter import request_adapter
 
 # Import composers clientes
-from src.main.composer.clientes.delete_cliente import delete_cliente_composer
-from src.main.composer.clientes.get_cliente_contatos import get_cliente_contatos_composer
-from src.main.composer.clientes.get_clientes import get_clientes_composer
 from src.main.composer.clientes.insert_cliente_contato import insert_cliente_contato_composer
+from src.main.composer.clientes.get_cliente_contatos import get_cliente_contatos_composer
+from src.main.composer.clientes.delete_cliente import delete_cliente_composer
 from src.main.composer.clientes.update_cliente import update_cliente_composer
+from src.main.composer.clientes.get_clientes import get_clientes_composer
 
 # Import composers contatos
+from src.main.composer.contatos.addcontato_to_cliente import add_contato_to_cliente_composer
+from src.main.composer.contatos.delete_contato import delete_contato_composer
 from src.main.composer.contatos.update_contato import update_contato_composer
 from src.main.composer.contatos.select_contato import select_contato_composer
 from src.main.composer.contatos.get_contatos import get_contatos_composer
-from src.main.composer.contatos.delete_contato import delete_contato_composer
-from src.main.composer.contatos.addcontato_to_cliente import add_contato_to_cliente_composer
 
+# Nomeando rotas
 cliente_route_bp = Blueprint("clientes_routes", __name__)
 contato_route_bp = Blueprint("contatos_routes", __name__)
 
@@ -30,15 +31,20 @@ def register_cliente_contato():
     try:
         return jsonify(http_response.body), http_response.status_code
 
-    except Exception:
-        return {"mensagem": Exception}
+
+    except Exception as exception:
+        return {"error": exception}
 
 
 @cliente_route_bp.route("/get/clientes", methods=["GET"])
 def find_clientes():
     try:
         http_response = request_adapter(request, get_clientes_composer())
-        return json.dumps({"body": str(http_response.body)})
+        return json.dumps(
+            {"body": str(http_response.body),
+             "status": str(http_response.status_code)
+             })
+
 
     except Exception as exception:
         return {"error": exception}
@@ -64,6 +70,7 @@ def delete_cliente():
     try:
         if http_response:
             return json.dumps({"request": http_response.body})
+
     except Exception as exception:
         return {"error": exception}
 
@@ -78,8 +85,6 @@ def update_cliente():
                 "body": str(body_lista),
                 "status_code": str(http_response.status_code)
             })
-
-
 
     except Exception as exception:
         return {"error": exception}
@@ -111,7 +116,6 @@ def select_contatos():
             })
 
 
-
     except Exception as exception:
         return {"error": exception}
 
@@ -126,9 +130,6 @@ def update_contatos():
                 "body": str(body_lista),
                 "status_code": str(http_response.status_code)
             })
-
-
-
 
 
     except Exception as exception:
