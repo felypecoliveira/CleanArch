cf162bf02123028a6855f8c496e4fab0a4df5276
Usuário <andrefelype2015@gmail.com>
2023-11-05 02:01:18 -0300

    added interface name in imports

--- src/presentation/controllers/clientes/get_cliente_contatos_controller.py
+++ src/presentation/controllers/clientes/get_cliente_contatos_controller.py
@@ -1,7 +1,7 @@
-from src.presentation.controllers.interface.controller import ControllerInterface as Interface
+from src.presentation.controllers.interface.controller_interface import ControllerInterface as Interface
 from src.presentation.http_types.http_request import HttpRequest
 from src.presentation.http_types.http_response import HttpResponse
-from src.interfaces.clientes.get_clientes_contatos import InterfaceGetClientesContatos
+from src.interfaces.clientes.get_clientes_contatos_interface import InterfaceGetClientesContatos
 
 
 class GetClientesContatosController(Interface):
