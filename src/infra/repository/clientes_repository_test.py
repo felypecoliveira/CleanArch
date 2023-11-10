8a24306c57f5e38639f21378310a1f511c33a51f
Usuário <andrefelype2015@gmail.com>
2023-11-10 07:48:03 -0300

    implementation test_insert_cliente_contato function

--- src/infra/repository/clientes_repository_test.py
+++ src/infra/repository/clientes_repository_test.py
@@ -1,5 +1,6 @@
 from src.infra.db.connect_settings import ConnectionHandler
 from clientes_repository import ClientesRepository
+from sqlalchemy import text
 
 db_connection_handler = ConnectionHandler()
 connection = db_connection_handler.get_engine().connect()
@@ -14,16 +15,65 @@ def select_cliente_by_name():
 
 
 def test_insert_cliente_contato():
+    # Clientes atrrs
+    mocked_nome = "First-"
+    mocked_telefone = "62985785500"
+    mocked_cpf = "05110918180"
+    mocked_endereco = "Av dos cantinhos serra alta"
+    mocked_data_nascimento = "1989-05-10"
+
+    # Contatos attrs
+
+    mocked_contato_nome = "Gustavo Mendes Souza"
+    mocked_telefone_contato = "62988452311"
+    mocked_email = "andorinhas123@gmail.com"
+
+    # Repository clientes
     clientes_repositorys = ClientesRepository()
 
+    clientes_repositorys.insert_cliente_contato(mocked_nome,
+                                                mocked_telefone,
+                                                mocked_cpf,
+                                                mocked_endereco,
+                                                mocked_data_nascimento,
+                                                mocked_contato_nome,
+                                                mocked_telefone_contato,
+                                                mocked_email)
 
-def get_clientes():
-    clientes_repositorys = ClientesRepository()
+    sql = f"""
+    SELECT  CL.NOME, 
+            CL.TELEFONE, 
+		    CL.CPF,  
+		    CLI.ENDERECO, 
+		    CL.DATA_NASCIMENTO,
+            CT.NOME_CONTATO,
+            CT.TELEFONE_CONTATO, 
+            CT.EMAIL
+            FROM CLIENTE
+            INNER JOIN CONTATO
+            ON CL.ID = CT.ID_CLIENTE;
+    """
 
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
 
-def get_clientes_contatos():
-    clientes_repositorys = ClientesRepository()
+    assert registry.nome == mocked_nome
+    assert registry.telefone == mocked_telefone
+    assert registry.cpf == mocked_cpf
+    assert registry.endereco == mocked_endereco
+    assert registry.data_nascimento == mocked_data_nascimento
+    assert registry.nome_contato == mocked_contato_nome
+    assert registry.telefone_contato == mocked_telefone_contato
+    assert registry.email == mocked_email
 
+    connection.execute(text(f" DELETE FROM CLIENTES WHERE ID == {registry.id}"))
+    connection.commit()
 
-def delete_cliente():
-    clientes_repositorys = ClientesRepository()
+    def get_clientes():
+        clientes_repositorys = ClientesRepository()
+
+    def get_clientes_contatos():
+        clientes_repositorys = ClientesRepository()
+
+    def delete_cliente():
+        clientes_repositorys = ClientesRepository()
