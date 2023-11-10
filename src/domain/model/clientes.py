5d67ee198e6bf88849a2bf203e81962dd8c91dc5
Usuário <andrefelype2015@gmail.com>
2023-11-10 07:58:21 -0300

    added method __init__

--- src/domain/model/clientes.py
+++ src/domain/model/clientes.py
@@ -2,9 +2,10 @@ from datetime import date
 
 
 class ClientesDominio:
-    id: int
-    nome: str
-    telefone: str
-    cpf: str
-    endereco: str
-    data_nascimento: date
+    def __init__(self, id: int, nome: str, telefone: str, cpf: str, endereco: str, data_nascimento: date):
+        self.id = id
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.endereco = endereco
+        self.data_nascimento = data_nascimento
--- src/domain/model/contatos.py
+++ src/domain/model/contatos.py
@@ -1,6 +1,7 @@
 class ContatosDominio:
-    id_contatos: int
-    nome_contato: str
-    telefone: str
-    email: str
-    id_cliente: int
+    def __init__(self, id_contatos: int, nome_contato: str, telefone_contato: str, email: str, id_cliente: int):
+        self.id_contatos = id_contatos
+        self.nome_contato = nome_contato
+        self.telefone_contato = telefone_contato
+        self.email = email
+        self.id_cliente = id_cliente
