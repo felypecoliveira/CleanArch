477a76215e31a042249683b988dd47c73873c5d3
Felype <109329430+felypecoliveira@users.noreply.github.com>
2023-11-14 21:37:33 -0300

    Delete LogicChallenges directory

--- LogicChallenges/becrowd/beginner/1009.py
+++
@@ -1,7 +0,0 @@
-nome = input()
-money_saller = float(input())
-total_sold = float(input())
-
-result = (total_sold * 0.15) + money_saller
-
-print("TOTAL = R$ {:.2f}".format(result))
--- LogicChallenges/becrowd/beginner/1019.py
+++
@@ -1,7 +0,0 @@
-segundos = int(input())
-horas = segundos // 3600
-resto_horas = segundos % 3600
-minutos = resto_horas // 60
-resto_minutos = segundos % 60
-
-print(f'{horas}:{minutos}:{resto_minutos}')
